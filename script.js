c81a556300f817cc8df49a94f20665eab45319af
BANI GIT <bani.rezaie@gmail.com>
2021-03-27 21:16:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(1+1)
+console.log('hellow world')
+
+console.log(Math.floor(Math.random()*10))
+
+// this is a comment
+
+// you can use : "node script.js" or just "node script" to watch what you are doing in terminal
+// you need to have node modules installed on your machine
